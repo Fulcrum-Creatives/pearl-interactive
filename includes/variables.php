1b27652dd5946d4e31210f8df64a28b3cbc9535f
jawittdesigns <jawittdesigns@gmail.com>
2015-04-10 11:35:37 -0400

    Added includes/variables.php

---
+++ includes/variables.php
@@ -0,0 +1,32 @@
+<?php
+/* Nav Menu Args
+================================================================================*/
+/**
+ * Mobile Icon element
+ * @var string
+ */
+$primary_mobile_icon = '<div id="menu__icon" class="menu__icon"><div class="inner"></div></div>' . "\n";
+/**
+ * Menu list wrapper
+ * @var string
+ */
+$primary_nav_list = '<ul id="%1$s" class="%2$s">%3$s</ul>';
+/**
+ * Menu wrapper
+ * @var string
+ */
+$primary_nav_wrapper = '<nav id="primary-nav" class="nav" role="navigation">' . "\n" . $primary_mobile_icon . $primary_nav_list . '</nav>';
+/**
+ * Primary Nav Args
+ * add class 'horizontal' to menu_class for a horizontal menu
+ * add class 'right', 'left', or 'center' to position horizontal menu
+ * @var array
+ */
+$primary_args = array(
+	'theme_location' => 'primary', 
+    'container'		 => '',
+    'menu_class'      => 'nav__menu horizontal center',
+    'menu_id'         => 'nav__menu',
+    'items_wrap'	 => $primary_nav_wrapper,
+    'walker'		 => new fc_wp_walker_nav_menu
+);
