8967cc1e0eb85afa366715e0344815bd3c2150d6
jawittdesigns <jawittdesigns@gmail.com>
2015-04-10 12:33:56 -0400

    Added Custom Nav Walker

--- functions.php
+++ functions.php
@@ -48,3 +48,60 @@ if( !function_exists( 'fcwpf_theme_support' ) ) :
 	}
 	add_action( 'after_setup_theme', 'fcwpf_theme_support' );
 endif;
+
+/* Custom Nav Walker
+================================================================================*/
+class fc_wp_walker_nav_menu extends Walker_Nav_Menu {
+	  
+	// add classes to ul sub-menus
+	function start_lvl( &$output, $depth ) {
+	    // depth dependent classes
+	    $indent = ( $depth > 0  ? str_repeat( "\t", $depth ) : '' ); // code indent
+	    $display_depth = ( $depth + 1); // because it counts the first submenu as 0
+	    $classes = array(
+		        'sub-menu',
+		        'menu-depth-' . $display_depth
+	        );
+	    $class_names = implode( ' ', $classes );
+	  
+	    // build html
+	    $output .= "\n" . $indent . '<ul class="' . $class_names . '">' . "\n";
+	}
+	  
+	// add main/sub classes to li's and links
+	function start_el( &$output, $item, $depth, $args ) {
+	    $indent = ( $depth > 0 ? str_repeat( "\t", $depth ) : '' ); // code indent
+	  	$display_depth = ( $depth + 1); // because it counts the first submenu as 0
+	    // depth dependent classes
+	    $depth_classes = array(
+	        ( $depth == 0 ? 'main-menu-item' : 'sub-menu-item' ),
+	        'menu-item-depth-' . $depth,
+	        'menu-item-' . strtolower( str_replace( ' ', '-', $item->title ) )
+	    );
+	    $depth_class_names = esc_attr( implode( ' ', $depth_classes ) );
+	  
+	    // passed classes
+	    $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+	    $class_names = esc_attr( implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) ) );
+	  
+	    // build html
+	    $output .= $indent . '<li id="nav-menu-item-'. $item->ID . '" class="' . $depth_class_names . ' ' . $class_names . '">';
+	  
+	    // link attributes
+	    $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+	    $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+	    $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+	    $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+	    $attributes .= ' class="menu-link ' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '"';
+	  
+	    
+	   	$item_output = $args->before;
+		$item_output .= '<a'. $attributes .'>';
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+	  
+	    // build html
+	    $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+}
